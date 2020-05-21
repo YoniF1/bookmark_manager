b6198404f0d489df7ba5874e61681ec0583203ce
Anna <anna.voitik@gmail.com>
2020-05-21 09:41:31 +0100

    Database wrapped in Bookmark object, tests pass

--- spec/add_bookmark_spec.rb
+++ spec/add_bookmark_spec.rb
@@ -1,8 +1,12 @@
+require 'bookmark'
+require 'database_helpers'
+
 describe Bookmark do
   describe '#add' do
     it 'adds a bookmark to the manager' do
       bookmark = Bookmark.add(url:"http://www.facebook.com", title:'Facebook')
-      persisted_data = PG.connect(dbname: 'bookmark_manager_test').query("SELECT * FROM bookmarks WHERE id = #{bookmark.id};")
+      persisted_data = persisted_data(id: bookmark.id)
+  #    persisted_data = PG.connect(dbname: 'bookmark_manager_test').query("SELECT * FROM bookmarks WHERE id = #{bookmark.id};")
       expect(bookmark).to be_a Bookmark
       expect(bookmark.id).to eq persisted_data.first['id']
       expect(bookmark.title).to eq 'Facebook'
--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -1,15 +1,19 @@
+require 'bookmark'
+require 'database_helpers'
+
 describe Bookmark do
   describe '#all' do
     it 'returns a list of bookmarks' do
-      Bookmark.add(url:"http://www.google.com", title: "Google")
-      Bookmark.add(url:"http://www.destroyallsoftware.com", title: "Destroyallsoftware")
-      Bookmark.add(url:"http://www.makersacademy.com", title: "Makers Academy")
+      connection = PG.connect(dbname: 'bookmark_manager_test')
+      bookmark = Bookmark.add(url: "http://www.makersacademy.com", title: "Makers Academy")
+      Bookmark.add(url: "http://www.destroyallsoftware.com", title: "Destroy All Software")
+      Bookmark.add(url: "http://www.google.com", title: "Google")
       bookmarks = Bookmark.all
       expect(bookmarks.length).to eq 3
       expect(bookmarks.first).to be_a Bookmark
-      # expect(bookmarks.first.id).to eq bookmarks.id
-      expect(bookmarks.first.title).to eq 'Google'
-      expect(bookmarks.first.url).to eq 'http://www.google.com'
+      expect(bookmarks.first.id).to eq bookmark.id
+      expect(bookmarks.first.title).to eq 'Makers Academy'
+      expect(bookmarks.first.url).to eq 'http://www.makersacademy.com'
     end
   end
 end
